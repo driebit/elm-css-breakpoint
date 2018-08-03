74e19152128bb0ae7d47240e391e0737df6c8350
rl-king <hello@rubenlieking.com>
2018-08-03 11:57:12 +0200

    Update docs

--- src/Css/Breakpoint.elm
+++ src/Css/Breakpoint.elm
@@ -1,9 +1,6 @@
 module Css.Breakpoint exposing (huge, large, medium, small, tiny)
 
-{-|
-
-
-# Breakpoints
+{-| _All breakpoints use `min-width` and match `all` media types._
 
 @docs tiny
 @docs small
@@ -17,31 +14,51 @@ import Css
 import Css.Media
 
 
-{-| -}
+{-|
+
+    @media (min-width: 480px)
+
+-}
 tiny : List Css.Style -> Css.Style
 tiny =
     custom 480
 
 
-{-| -}
+{-|
+
+    @media (min-width: 768px)
+
+-}
 small : List Css.Style -> Css.Style
 small =
     custom 768
 
 
-{-| -}
+{-|
+
+    @media (min-width: 1024px)
+
+-}
 medium : List Css.Style -> Css.Style
 medium =
     custom 1024
 
 
-{-| -}
+{-|
+
+    @media (min-width: 1280px)
+
+-}
 large : List Css.Style -> Css.Style
 large =
     custom 1280
 
 
-{-| -}
+{-|
+
+    @media (min-width: 1660px)
+
+-}
 huge : List Css.Style -> Css.Style
 huge =
     custom 1660
